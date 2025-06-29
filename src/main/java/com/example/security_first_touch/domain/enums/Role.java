9326e84b35db8eb2b0d927bf6ec7036e11b2b686
amir_java <79984458+amirgilmanov@users.noreply.github.com>
2025-06-29 22:50:35 +0500

    Update Role.java

--- src/main/java/com/example/security_first_touch/domain/enums/Role.java
+++ src/main/java/com/example/security_first_touch/domain/enums/Role.java
@@ -1,1 +1,15 @@
-package com.example.security_first_touch.domain.enums;import lombok.Getter;import org.springframework.security.core.GrantedAuthority;@Getterpublic enum Role implements GrantedAuthority {    USER,    ADMIN;    @Override    public String getAuthority() {        return name();    }}
+package com.example.security_first_touch.domain.enums;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+public enum Role implements GrantedAuthority {
+    USER,
+    ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
