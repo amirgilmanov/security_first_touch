0864f3534bb5c7d004e4514e189ecde8fb6f2c21
amir_java <79984458+amirgilmanov@users.noreply.github.com>
2025-06-29 22:50:48 +0500

    Update RoleRepository.java

--- src/main/java/com/example/security_first_touch/domain/repository/RoleRepository.java
+++ src/main/java/com/example/security_first_touch/domain/repository/RoleRepository.java
@@ -1,1 +1,12 @@
-package com.example.security_first_touch.domain.repository;import com.example.security_first_touch.domain.entity.Role;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.stereotype.Repository;import java.util.Optional;@Repositorypublic interface RoleRepository  extends JpaRepository<Role, Long> {    Optional<Role> findByName(String name);}
+package com.example.security_first_touch.domain.repository;
+
+import com.example.security_first_touch.domain.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
