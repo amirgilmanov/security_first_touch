07759d8b4b662403f674e862b2eac0a09b4a6141
amir_java <79984458+amirgilmanov@users.noreply.github.com>
2025-06-29 22:50:05 +0500

    Update Role.java

--- src/main/java/com/example/security_first_touch/domain/entity/Role.java
+++ src/main/java/com/example/security_first_touch/domain/entity/Role.java
@@ -1,1 +1,21 @@
-package com.example.security_first_touch.domain.entity;import jakarta.persistence.*;import lombok.AllArgsConstructor;import lombok.Getter;import lombok.NoArgsConstructor;import lombok.Setter;@Getter@Setter@Entity@Table(name = "roles")@AllArgsConstructor@NoArgsConstructorpublic class Role {    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)    private Long id;    private String name;}
+package com.example.security_first_touch.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
