2abd83e614f35e98f35c7899fc91eee6e0e715ad
amir_java <79984458+amirgilmanov@users.noreply.github.com>
2025-06-29 22:49:52 +0500

    Update UserDto.java

--- src/main/java/com/example/security_first_touch/domain/dto/UserDto.java
+++ src/main/java/com/example/security_first_touch/domain/dto/UserDto.java
@@ -1,1 +1,12 @@
-package com.example.security_first_touch.domain.dto;import lombok.AllArgsConstructor;import lombok.Data;@Data@AllArgsConstructorpublic class UserDto {    private Long id;    private String username;    private String email;}
+package com.example.security_first_touch.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String email;
+}
