2ccdd1deccba00d22f2124cf8f648849fb01c8e7
amir_java <79984458+amirgilmanov@users.noreply.github.com>
2025-06-29 22:49:26 +0500

    Update LogInDto.java

--- src/main/java/com/example/security_first_touch/domain/dto/LogInDto.java
+++ src/main/java/com/example/security_first_touch/domain/dto/LogInDto.java
@@ -1,1 +1,7 @@
-package com.example.security_first_touch.domain.dto;public record LogInDto(        String username,        String password) {}
+package com.example.security_first_touch.domain.dto;
+
+public record LogInDto(
+        String username,
+        String password
+) {
+}
