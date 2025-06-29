81257362b3923ed54c7ec53a0784bedb227231a9
amir_java <79984458+amirgilmanov@users.noreply.github.com>
2025-06-29 22:51:34 +0500

    Update RoleService.java

--- src/main/java/com/example/security_first_touch/service/RoleService.java
+++ src/main/java/com/example/security_first_touch/service/RoleService.java
@@ -1,1 +1,16 @@
-package com.example.security_first_touch.service;import com.example.security_first_touch.domain.entity.Role;import com.example.security_first_touch.domain.repository.RoleRepository;import lombok.RequiredArgsConstructor;import org.springframework.stereotype.Service;@Service@RequiredArgsConstructorpublic class RoleService {    private final RoleRepository roleRepository;    public Role findRoleByName(String name) {        return roleRepository.findByName(name).orElseThrow(() -> new RuntimeException("Role not found"));    }}
+package com.example.security_first_touch.service;
+
+import com.example.security_first_touch.domain.entity.Role;
+import com.example.security_first_touch.domain.repository.RoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public Role findRoleByName(String name) {
+        return roleRepository.findByName(name).orElseThrow(() -> new RuntimeException("Role not found"));
+    }
+}
